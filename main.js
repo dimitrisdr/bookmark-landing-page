1ecd5599ae99aa919ada2ae0ccae6debadb9b7f5
Dimitris_Dr <jidrist18@hotmail.com>
2024-09-19 14:48:37 +0300

    update

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ function checkIfEmailValid(email, regexPattern=invalidRegexEmailPattern){
 }
 
 // event listeners
+
 featureLinks.forEach((link, i) => link.addEventListener('click', ()=> {
     featureLinks.forEach(fLink => fLink.parentElement.setAttribute('aria-checked', 'false'))
     link.parentElement.setAttribute('aria-checked', 'true')
