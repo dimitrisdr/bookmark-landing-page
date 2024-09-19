437d69719a4821156b79619c8f6147867933fa7c
Dimitris_Dr <jidrist18@hotmail.com>
2024-09-19 14:48:02 +0300

    main update

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+// variables
+
+const invalidRegexEmailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
 let featureLinks = document.querySelectorAll('.features-item__link')
 let featureItems = document.querySelectorAll('.features-list-categories__item')
 let form = document.querySelector('.form')
+let emailInput = document.querySelector('.form-control__email-input')
+let errorStateElements = document.querySelectorAll('.error-icon, .error-msg')
 
+// functions
+
+function checkIfEmailValid(email, regexPattern=invalidRegexEmailPattern){
+    return regexPattern.test(email)
+}
+
+// event listeners
 featureLinks.forEach((link, i) => link.addEventListener('click', ()=> {
     featureLinks.forEach(fLink => fLink.parentElement.setAttribute('aria-checked', 'false'))
     link.parentElement.setAttribute('aria-checked', 'true')
@@ -13,5 +26,6 @@ featureLinks.forEach((link, i) => link.addEventListener('click', ()=> {
 form.addEventListener('submit', (e)=> {
     e.preventDefault()
     const data = Object.fromEntries(new FormData(e.target))
-    console.log(data)
+    const isEmailValid = checkIfEmailValid(data['email']) && ''
+    errorStateElements.forEach(el => el.setAttribute('aria-hidden', (isEmailValid).toString()))
 })
