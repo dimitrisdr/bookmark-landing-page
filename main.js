3a16cab95b59bcb2dd97810f9e86a4547b49efaf
Dimitris_Dr <jidrist18@hotmail.com>
2024-09-19 18:15:06 +0300

    changed aria-labels

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ function checkIfEmailValid(email, regexPattern=invalidRegexEmailPattern){
 // event listeners
 
 featureLinks.forEach((link, i) => link.addEventListener('click', ()=> {
-    featureLinks.forEach(fLink => fLink.parentElement.setAttribute('aria-checked', 'false'))
-    link.parentElement.setAttribute('aria-checked', 'true')
+    featureLinks.forEach(fLink => fLink.parentElement.setAttribute('data-checked', 'false'))
+    link.parentElement.setAttribute('data-checked', 'true')
     featureItems.forEach(item => item.setAttribute('aria-hidden', 'true'))
     featureItems[i].setAttribute('aria-hidden', 'false')
 }))
